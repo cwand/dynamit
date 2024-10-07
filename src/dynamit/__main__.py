91fad600760c1ab0e3a6260c96aeb78e230d616b
Christian Walther Andersen <cvvand@gmail.com>
2024-10-07 14:20:08 +0200

    Some extraction test implemented

--- src/dynamit/__main__.py
+++ src/dynamit/__main__.py
@@ -1,8 +1,23 @@
-import dynamit
+import SimpleITK as sitk
 
 
 def main():
-    print(f"1 + 2 = {dynamit.add(1, 2)}.")
+    image = sitk.ReadImage("C:\\Users\\bub8ga\\data\\dynamit-i\\KTH\\NM73\\NM000037.dcm")
+
+    roi = sitk.ReadImage("C:\\Users\\bub8ga\\data\\dynamit-i\\KTH-seg\\kthseg.nii")
+
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(image)
+    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+    resampler.SetOutputPixelType(roi.GetPixelID())
+    resampled_roi = resampler.Execute(roi)
+
+    label_stats_filter = sitk.LabelStatisticsImageFilter()
+    label_stats_filter.Execute(image, resampled_roi)
+
+    for label in label_stats_filter.GetLabels():
+        print(f"Label: {label}")
+        print(f"Mean ROI-value: {label_stats_filter.GetMean(label)}")
 
 
 if __name__ == "__main__":
